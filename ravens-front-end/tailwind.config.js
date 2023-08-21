4fd207402e09bda0e1c24b2171be19b595c9baa7
Mia Coupland <miacoupland@btinternet.com>
2023-08-21 19:53:18 +0100

    Manage fonts

--- ravens-front-end/tailwind.config.js
+++ ravens-front-end/tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'yellow': '#fdc010',
+      'black': '#000000',
+      'white': '#FFFFFF',
+      'red': '#e11b22'
+    },
+    fontFamily: {
+      'sans': ['Scorno'],
+      'scorno': ['Scorno'],
+    },
     extend: {},
   },
   plugins: [],
