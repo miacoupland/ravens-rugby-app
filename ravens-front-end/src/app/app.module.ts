72a13a49dc9f4b8cd0879d4684746284aa76d447
Mia Coupland <miacoupland@btinternet.com>
2023-08-21 19:52:56 +0100

    Add header and footer

--- ravens-front-end/src/app/app.module.ts
+++ ravens-front-end/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SignUpFormComponent } from './presentation/sign-up-form/sign-up-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './presentation/header/header.component';
+import { FooterComponent } from './presentation/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpFormComponent
+    SignUpFormComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
